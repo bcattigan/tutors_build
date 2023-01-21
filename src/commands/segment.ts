0889ce158a107ff8ce04e63e0f5e0d82b17387c9
bcattigan <20095670@mail.wit.ie>
2023-01-21 23:44:52 +0000

    Work complete up to: 21-01-2023
    Feature: build_unit

--- src/commands/segment.ts
+++ src/commands/segment.ts
@@ -1,8 +1,15 @@
 import chalk from "chalk";
+import path from "path";
 import { commonPrompts } from "../prompts/common_prompts";
+import { getFolderCountOfType, writeToTemplate } from "../utils/utils";
 
 export async function buildSegment() {
-  console.log(chalk.greenBright("Build a segment:"));
-  const segmentObj = await commonPrompts.titleAndDesc("Segment", true);
-  console.log(chalk.red(`Title: ${segmentObj.title}`));
+  if (path.basename(process.cwd()).startsWith("topic")) {
+    console.log(chalk.greenBright("Build a segment:"));
+    const segmentObj = await commonPrompts.titleAndDesc("Segment", true);
+    const folderName = `${getFolderCountOfType("unit")}-${segmentObj.title}`;
+    writeToTemplate("segment/segment.md", segmentObj, folderName);
+  } else {
+    console.log(chalk.red("Error: Segments can only be created at topic level"));
+  }
 }
